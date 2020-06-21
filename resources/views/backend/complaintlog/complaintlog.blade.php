f9f5375eb397c7573f2652065c8da84eef9c4f80
Naijabas <basitibitoye@gmail.com>
2020-06-21 22:56:03 +0100

    latest complaint form and log

--- resources/views/backend/complaintlog/complaintlog.blade.php
+++ resources/views/backend/complaintlog/complaintlog.blade.php
@@ -10,14 +10,13 @@
 
     @section('content')
 
-   <div class="container" style="font-family: 'Inter', sans-serif;">
+   <div style="font-family: 'Inter', sans-serif; background-color: white; padding: 10px; margin-top: 10px; border-radius: 10px;">
         <table class="highlight">
         <thead>
           <tr>
-              <th>S/N</th>
-              <th style="width: 10%;">Name</th>
-              <th>Email Address</th>
+              <th>User ID</th>
               <th>Message</th>
+              <th>Store Reference Code</th>
               <th>Status</th>
               <th style="width: 10%;">Date</th>
           </tr>
@@ -26,57 +25,35 @@
         <tbody>
           <tr>
             <td>1</td>
-            <td>Alan Jellybean</td>
-            <td>alan@gmail.com</td>
             <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</td>
+            <td>ST145M455</td>
             <td>Closed</td>
             <td>2020-06-23 13:34:67</td>
           </tr>
           <tr>
-            <td>2</td>
-            <td>Alvin Eclair</td>
-            <td>eclair@gmail.com</td>
+            <td>1</td>
             <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</td>
+            <td>ST145M455</td>
             <td>Closed</td>
-            <td>2020-05-23 23:17:67</td>
+            <td>2020-06-23 13:34:67</td>
           </tr>
           <tr>
-            <td>3</td>
-            <td>Alvin Eclair</td>
-            <td>eclair@gmail.com</td>
-            <!-- <td>Closed</td> -->
+            <td>1</td>
             <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</td>
+            <td>ST145M455</td>
             <td>Closed</td>
-            <td>2020-05-23 13:67:67</td>
+            <td>2020-06-23 13:34:67</td>
           </tr>
           <tr>
-            <td>4</td>
-            <td>Jonathan Lollipop</td>
-            <td>jonathan@gmail.com</td>
-            <!-- <td>Open</td> -->
+            <td>1</td>
             <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</td>
+            <td>ST145M455</td>
             <td>Closed</td>
-            <td>2020-05-23 13:67:67</td>
+            <td>2020-06-23 13:34:67</td>
           </tr>
         </tbody>
       </table>
