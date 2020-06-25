331d71b7c8e3271b37e2a188827993a2db1cb3ac
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-06-25 17:13:50 +0100

    tokyo implement user list

--- routes/web.php
+++ routes/web.php
@@ -63,9 +63,11 @@ Route::get('/backend/view_transaction', function () {
 
 
 
-Route::get('/backend/users', function () {
-    return view('backend.users_list.index');
-});
+// Route::get('/backend/users', function () {
+//     return view('backend.users_list.index');
+// });
+Route::resource('/backend/users', 'UsersController');
+
 
 Route::get('/backend/debt_reminders', function () {
     return view('backend.debt_reminder.index');
