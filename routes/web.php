db6de39edc4f4cb1bb966947b04e25a02ed7f9de
kay-ai <favourobasi6@gmail.com>
2020-06-26 00:42:46 +0100

    Auth Guard for Routes

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/faq', function () {
 
 Route::get('/backend/login', function () {
     return view('backend.login');
-});
+})->name('login');
 
 Route::get('/backend/register', function () {
     return view('backend.register.signup');
@@ -51,59 +51,65 @@ Route::get('backend/activate', function () {
     return view('backend.activate.activate');
 });
 
-// dashboard
-Route::get('/backend/dashboard', function () {
-    return view('backend.dashboard');
+
+//Protected Routes
+Route::middleware(['auth'])->group(function(){
+
+    // dashboard
+    Route::get('/backend/dashboard', function () {
+        return view('backend.dashboard');
+    });
+
+    // transaction
+
+    Route::get('/backend/transactions', function () {
+        return view('backend.transactions.index');
+    });
+
+    Route::get('/backend/view_transaction', function () {
+        return view('backend.transactions.show');
+    });
+
+
+
+    // Route::get('/backend/users', function () {
+    //     return view('backend.users_list.index');
+    // });
+    Route::resource('/backend/users', 'UsersController');
+
+
+    Route::get('/backend/debt_reminders', function () {
+        return view('backend.debt_reminder.index');
+    });
+
+
+    Route::get('/backend/complaint', function () {
+        return view('backend.complaintform.complaintform');
+    });
+
+    Route::get('/backend/complaint_log', function () {
+        return view('backend.complaintlog.complaintlog');
+    });
+
+    // all users
+
+    Route::get('/users_list', function () {
+        return view('users_list.single_user');
+    });
+
+    Route::get('/backend/view_user', function () {
+        return view('backend.users_list.show');
+    });
+
+    // analytics
+    Route::get('/backend/analytics', function () {
+        return view('backend.analytics.analytics');
+    })->name('analytics');
+
+
+    // settings
+    Route::get('/backend/settings', function () {
+        return view('backend.settings.settings');
+    })->name('settings');
+
 });
-
-// transaction
-
-Route::get('/backend/transactions', function () {
-    return view('backend.transactions.index');
-});
-
-Route::get('/backend/view_transaction', function () {
-    return view('backend.transactions.show');
-});
-
-
-
-// Route::get('/backend/users', function () {
-//     return view('backend.users_list.index');
-// });
-Route::resource('/backend/users', 'UsersController');
-
-
-Route::get('/backend/debt_reminders', function () {
-    return view('backend.debt_reminder.index');
-});
-
-
-Route::get('/backend/complaint', function () {
-    return view('backend.complaintform.complaintform');
-});
-
-Route::get('/backend/complaint_log', function () {
-    return view('backend.complaintlog.complaintlog');
-});
-
-// all users
-
-Route::get('/users_list', function () {
-    return view('users_list.single_user');
-});
-
-Route::get('/backend/view_user', function () {
-    return view('backend.users_list.show');
-});
-
-// analytics
-Route::get('/backend/analytics', function () {
-    return view('backend.analytics.analytics');
-})->name('analytics');
-
-
-// settings
-Route::get('/backend/settings', function () {
-    return view('backend.settings.settings');
-})->name('settings');
