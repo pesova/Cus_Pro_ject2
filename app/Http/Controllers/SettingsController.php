b1685825e159b74efab16c02d46f3df60887fc22
Eniola Osabiya <eosabiya@gmail.com>
2020-06-26 13:03:46 +0100

    changed {{route('settings')}} to  backend/settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -35,79 +35,80 @@ class SettingsController extends Controller
 
     // Controller action to update user details.
     public function update(Request $request) {
-        // try{
+        try{
 
             // This is the user_id and should be gotten from session
             $user_id = '5eee79d20699014986af4201';
 
-            // check if all fields are available
+            // API NOT WORKING ONLINE YET: NODE
+            // ==============================================
+                // check if all fields are available
+                // if($request->all()) {
 
-                // API NOT WORKING ONLINE YET
-                        // if($request->all()) {
+                //     $control = $request->input('control', '');
 
-                        //     $control = $request->input('control', '');
+                //     if ($control == 'profile_update') {
 
-                        //     if ($control == 'profile_update') {
+                //         // make an api call to update the user_details
+                //         $client = new Client();
+                //         $response = $client->put(env('API_URL') . '/user/update/' . $user_id, [
+                //             'form_params' => [
+                //                 'name' => $request->input('first_name'),
+                //                 'lastname' => $request->input('last_name'),
+                //                 'email' => $request->input('email')
+                //             ]
+                //         ]);
 
-                        //         // make an api call to update the user_details
-                        //         $client = new Client();
-                        //         $response = $client->put(env('API_URL') . '/user/update/' . $user_id, [
-                        //             'form_params' => [
-                        //                 'name' => $request->input('first_name'),
-                        //                 'lastname' => $request->input('last_name'),
-                        //                 'email' => $request->input('email')
-                        //             ]
-                        //         ]);
+                //     } elseif ($control == 'password_change') {
 
-                        //     } elseif ($control == 'password_change') {
+                //         // make an api call to update the user_details
+                //         $client = new Client();
+                //         $response = $client->post(env('API_URL') . '/reset-password', [
+                //             'form_params' => [
+                //                 'current_password' => $request->input('current_password'),
+                //                 'new_password' => $request->input('new_password')
+                //             ]
+                //         ]);
 
-                        //         // make an api call to update the user_details
-                        //         $client = new Client();
-                        //         $response = $client->post(env('API_URL') . '/reset-password', [
-                        //             'form_params' => [
-                        //                 'current_password' => $request->input('current_password'),
-                        //                 'new_password' => $request->input('new_password')
-                        //             ]
-                        //         ]);
+                //     } else {
 
-                        //     } else {
+                //         return view('errors.500');
+                //     }
 
-                        //         return view('errors.500');
-                        //     }
+                //     // api call to get the updated user info
+                //     $client = new Client();
+                //     $user_detail = $client->get(env('API_URL') . '/user/' . $user_id);
+                //     $user_detail_res = json_decode($user_detail->getBody(), true);
 
-                        //     // api call to get the updated user info
-                        //     $client = new Client();
-                        //     $user_detail = $client->get(env('API_URL') . '/user/' . $user_id);
-                        //     $user_detail_res = json_decode($user_detail->getBody(), true);
+                //     if ($response->getStatusCode() == 201) {
 
-                        //     if ($response->getStatusCode() == 201) {
+                //         $res = json_decode($response->getBody(), true);
 
-                        //         $res = json_decode($response->getBody(), true);
+                //         return view('backend.settings.settings')->with([
+                //             'user_details' => $res,
+                //             'form_response' => $user_detail_res
+                //         ]);
+                //     }
 
-                        //         return view('backend.settings.settings')->with([
-                        //             'user_details' => $res,
-                        //             'form_response' => $user_detail_res
-                        //         ]);
-                        //     }
+                //     if ($response->getStatusCode() == 400) {
 
-                        //     if ($response->getStatusCode() == 400) {
+                //         $res = json_decode($response->getBody(), true);
 
-                        //         $res = json_decode($response->getBody(), true);
+                //         return view('backend.settings.settings')->with([
+                //             'user_details' => $res,
+                //             'form_response' => $user_detail_res
+                //         ]);
+                //     }
 
-                        //         return view('backend.settings.settings')->with([
-                        //             'user_details' => $res,
-                        //             'form_response' => $user_detail_res
-                        //         ]);
-                        //     }
-
-                        //     if ($response->getStatusCode() == 500) {
-                        //         return view('errors.500');
-                        //     }
-                        // } else {
-                        //     return redirect('/backend/settings');
-                        // }
-        // } catch(\Exception $e) {
-        //     return view('errors.500');
-        // }
+                //     if ($response->getStatusCode() == 500) {
+                //         return view('errors.500');
+                //     }
+                // } else {
+                //     return redirect('/backend/settings');
+                // }
+            // ==============================================
+        } catch(\Exception $e) {
+            return view('errors.500');
+        }
     }
 }
