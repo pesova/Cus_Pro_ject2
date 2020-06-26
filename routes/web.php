9cf010cbc8b8b0c7e905016e5a70a2a224c4ebb4
Kadet <techkadet@gmail.com>
2020-06-26 21:10:23 +0100

    corrected colors on the homepage

--- routes/web.php
+++ routes/web.php
@@ -102,7 +102,6 @@ Route::get('/backend/analytics', function () {
 
 
 // settings
-<<<<<<< HEAD
 Route::get('/backend/settings', function () {
     return view('backend.settings.settings');
 })->name('settings');
@@ -111,7 +110,6 @@ Route::get('/backend/settings', function () {
 Route::get('/backend/stores', function () {
   return view('backend.stores.store_list');
 });
-=======
 
 // Route::get('/backend/settings', function () {
 //     return view('backend.settings.settings');
@@ -121,4 +119,3 @@ Route::get('/backend/settings', 'SettingsController@index');
 
 Route::post('/backend/settings', 'SettingsController@update')->name('settings');
 
->>>>>>> aed16736bd1fd91ba2c6a7522f3b9b7fde448699
