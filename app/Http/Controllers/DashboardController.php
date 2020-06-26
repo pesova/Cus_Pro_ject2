4b6a731ee3e6e7d3c7c7478aa37138b18e177c91
Ezeko <ezeko2017@gmail.com>
2020-06-26 12:47:35 +0100

    feat
    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class DashboardController extends Controller
 {
     //
-   public function dash () {
-       
-        return view('backend.dashboard');//->with(['']);
+   public function dash (Request $request) {
+        $token = $request->cookie('api_token');
+        $response = Http::get( 'https://dev.customerpay.me/transaction/all', ['api_token' => $token]);
+        $statusCode = $response->getStatusCode();
+        $body = $response->getBody()->getContents();
+        $transactions = json_decode($body);
+        if ($statusCode === 200){
+            return view('backend.dashboard')->with(['transactions' => $transactions]);
+
+        }else{
+            //return 404 page
+            return view('errors.404');
+        }
+        
+    
     }
 }
