29a7239dfc1e2adfb9dd68276ba286ce16155f36
Isiko saidiali <isikosaidiali@gmail.com>
2020-06-28 20:49:38 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('home');
+});
+
+Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
