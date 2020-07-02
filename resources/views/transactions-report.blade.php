eb810a08758c144c3b214ddaa994e5ac8e33e60a
Prince <chimeremezechukwudire@gmail.com>
2020-07-02 12:04:08 +0100

    side bar optimized

--- resources/views/transactions-report.blade.php
+++ resources/views/transactions-report.blade.php
@@ -16,8 +16,16 @@
                     <h5 class="card-title">Transactions Report</h5>
                     <p class="card-text"><span>JohnDoe Enterprises</span></p>
                     <p class="card-textx mb-5">For <span>July 1, 2020 - July 30, 2020</span></p> 
+                        <div class="row">
 
-                    <p class='receivables'>Accounts Receivables (1200-1)</p>
+                            <div class="col">
+                            <p class='receivables'>Accounts Receivables (1200-1)</p>
+                            </div>
+                            <!-- <div class="col"></div>
+                            <div class="col"></div> -->
+                            <div class="col"><a href="#" class="btn btn-primary">Button</a></div>
+                        </div>
+            
                     <!-- <hr class='border-line'> -->
                         <table class="table table-responsive-sm table-responsive-md table-hover">
                             <thead>
@@ -57,7 +65,7 @@
                                 </tr>
                             </tbody>
                         </table>
-                        <a href="#" class="btn btn-primary">Button</a>
+                      
                 </div>
                 </div>
             </div>
@@ -66,43 +74,39 @@
                     <div class="col-md-6">
                         Filters
                     </div>
-                    <div class="col-md-6">
-                        Reset all
-                    </div>
+                 
                 </div>
                 <form action="">
                     <div class="form-group">
                         <label for="date_range" class="savings-label mt-4">  Date Range</label>
-                        <select  class="select2 form-control" id="date_range" required >    
-                              <option value="10days">10 - 30 days</option>
-                              <option value="31days">31 - 60 days</option>  
-                              <option value="61 days">61 - 90 days</option>
-                              <option value="91days">91days to 2years</option>
-                              <option value="2years">Over 2 years</option>        
+                        <select  class=" form-control" id="date_range" required >    
+                              <option value="">10 - 30 days</option>
+                              <option value="">31 - 60 days</option>  
+                              <option value="">61 - 90 days</option>
+                              <option value="">91days to 2years</option>
+                              <option value="">Over 2 years</option>        
                           </select>
                     </div>
 
                     <div class="form-group">
-                        <label for="date_range" class="savings-label mt-3">  Date Range</label>
-                        <select  class="select2 form-control" id="date_range" required >    
-                              <option value="10days">10 - 30 days</option>
-                              <option value="31days">31 - 60 days</option>  
-                              <option value="61 days">61 - 90 days</option>
-                              <option value="91days">91days to 2years</option>
-                              <option value="2years">Over 2 years</option>        
+                        <label for="currency" class="savings-label mt-3">   Currency</label>
+                        <select  class=" form-control" id="currency" required >    
+                              <option value="">NAIRA</option>
+                              <option value="">USD</option>  
+                              <option value=" ">YEN</option>
+                                  
                           </select>
                     </div>
 
                     <div class="form-group">
-                        <label for="date_range" class="savings-label mt-3">  Date Range</label>
-                        <select  class="select2 form-control" id="date_range" required >    
-                              <option value="10days">10 - 30 days</option>
-                              <option value="31days">31 - 60 days</option>  
-                              <option value="61 days">61 - 90 days</option>
-                              <option value="91days">91days to 2years</option>
-                              <option value="2years">Over 2 years</option>        
+                        <label for="limit" class="savings-label mt-3">  Limit to</label>
+                        <select  class=" form-control" id="limit" required >    
+                              <option value="">All Accounts</option>
+                              <option value="">Debit</option>  
+                              <option value="">Credit</option>     
                           </select>
                     </div>
+                    <button type="submit" class="btn btn-outline-dark">Apply</button>
 
                 </form>
             </div>
