751fe43eec19b7c9722e928cae8e61eaf53e5c2c
Prince <chimeremezechukwudire@gmail.com>
2020-07-02 11:23:36 +0100

    side bar added

--- resources/views/transactions-report.blade.php
+++ resources/views/transactions-report.blade.php
@@ -10,41 +10,50 @@
 <section class="container report">
     <div>
         <div class="row">
-            <div class="col-md-8 col-lg-8">
+            <div class="col-md-9 col-lg-9">
             <div class="card 5">
                 <div class="card-body">
                     <h5 class="card-title">Transactions Report</h5>
                     <p class="card-text"><span>JohnDoe Enterprises</span></p>
                     <p class="card-textx mb-5">For <span>July 1, 2020 - July 30, 2020</span></p> 
 
-                    <p >Accounts Receivables (1200-1)</p>
-                    <hr class='border-line'>
+                    <p class='receivables'>Accounts Receivables (1200-1)</p>
+                    <!-- <hr class='border-line'> -->
                         <table class="table table-responsive-sm table-responsive-md table-hover">
                             <thead>
-                                <tr>
+                                <tr >
                                 <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Transaction/Reference</th>
+                                <th scope="col">Client</th>
+                                <th scope="col">Debit</th>
+                                <th scope="col">Credit</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                 <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
+                                <td>01-01-20</td>
+                                <td>JD-0001245</td>
+                                <td>Jane Doe</td>
+                                <td>#100</td>
+                                <td></td>
                                 </tr>
                                 <tr>
                                 <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
+                                <td>01-01-20</td>
+                                <td>JD-0001245</td>
+                                <td>Jane Doe</td>
+                                <td>#100</td>
+                                <td></td>
                                 </tr>
                                 <tr>
                                 <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
+                                <td>01-01-20</td>
+                                <td>JD-0001245</td>
+                                <td>Jane Doe</td>
+                                <td>#100</td>
+                                <td></td>
                                 </tr>
                             </tbody>
                         </table>
@@ -52,8 +61,50 @@
                 </div>
                 </div>
             </div>
-            <div class="col-md-4 col-lg-4">
-            2 of 2
+            <div class="col-md-3 col-lg-3">
+                <div class="row">
+                    <div class="col-md-6">
+                        Filters
+                    </div>
+                    <div class="col-md-6">
+                        Reset all
+                    </div>
+                </div>
+                <form action="">
+                    <div class="form-group">
+                        <label for="date_range" class="savings-label mt-4">  Date Range</label>
+                        <select  class="select2 form-control" id="date_range" required >    
+                              <option value="10days">10 - 30 days</option>
+                              <option value="31days">31 - 60 days</option>  
+                              <option value="61 days">61 - 90 days</option>
+                              <option value="91days">91days to 2years</option>
+                              <option value="2years">Over 2 years</option>        
+                          </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="date_range" class="savings-label mt-3">  Date Range</label>
+                        <select  class="select2 form-control" id="date_range" required >    
+                              <option value="10days">10 - 30 days</option>
+                              <option value="31days">31 - 60 days</option>  
+                              <option value="61 days">61 - 90 days</option>
+                              <option value="91days">91days to 2years</option>
+                              <option value="2years">Over 2 years</option>        
+                          </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="date_range" class="savings-label mt-3">  Date Range</label>
+                        <select  class="select2 form-control" id="date_range" required >    
+                              <option value="10days">10 - 30 days</option>
+                              <option value="31days">31 - 60 days</option>  
+                              <option value="61 days">61 - 90 days</option>
+                              <option value="91days">91days to 2years</option>
+                              <option value="2years">Over 2 years</option>        
+                          </select>
+                    </div>
+
+                </form>
             </div>
         </div>
     </div>
