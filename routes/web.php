5497cb7a56cc9efa421515ec47a7a9398820e206
Yusuf Abdulrahman <48928718+devYusuf@users.noreply.github.com>
2020-07-02 15:13:32 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -211,6 +211,6 @@ Route::get('/backend/1123', function () {
         return view('backend.notifications.user_notification');
     })->name('notification');
     
-    Route::get('/complaint/update/{id}' , 'ComplaintlogController@update');
+    Route::get('/complaint_log/update/{id}' , 'ComplaintlogController@update');
 
 });
