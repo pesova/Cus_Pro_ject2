20033c35122376bc5ca59c1ff443eb7bda90b2ff
orlawealth <luv4junior@yahoo.com>
2020-07-03 09:42:32 +0100

    modified usersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -112,6 +112,32 @@ class UsersController extends Controller
      */
     public function destroy($id)
     {
-        //
+       // return view('backend.dashboard.index');
+       $host = env('API_URL', 'https://dev.api.customerpay.me/');
+       $url = $host."/user/delete/$id";
+       // return $url;
+       try {
+           $client = new Client();
+           $headers = ['headers' => ['x-access-token' => Cookie::get('api_token')]];
+           $response = $client->request('GET', $url, $headers);
+           $statusCode = $response->getStatusCode();
+           if ($statusCode == 200) {
+               $body = $response->getBody()->getContents();
+               $transactions = json_decode($body);
+               return view('backend.dashboard.index')->with('response', $transactions);
+           }
+           if ($statusCode == 500) {
+               return view('errors.500');
+           }
+           if ($statusCode == 401) {
+               return view('backend.dashboard.index')->with('error', "Unauthoized toke");
+           }
+           if ($statusCode == 404) {
+               return view('backend.dashboard.index')->with('error', "User not found");
+           }
+       } catch (\Exception $e) {
+           // return view('errors.500');
+           return view('backend.dashboard.index')->with('error', "Unable to connect to server");
+       }
     }
 }
