cfb3ad8ae66eb0ca3648042999385d106289fd1f
Jeremiah Iro <Jeremiahiro@gmail.com>
2020-07-03 12:34:23 +0100

    Update frontbase.blade.php

--- resources/views/layout/frontbase.blade.php
+++ resources/views/layout/frontbase.blade.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, shrink-to-fit=no">
-    <meta content="MyCustomer - HNGi7" name="description" />
+    <meta content="MyCustomer - JusticeLeague" name="description" />
     <meta content="JusticeLeague" name="author" />
     <title>MyCustomer</title>
 
