69f52591968b356f24d55bf83f18fcc1fbf21174
jeremiahiro <jeremiahiro@gmail.com>
2020-07-09 00:41:15 +0100

    fixed logo redirect

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -17,7 +17,7 @@
                         <div class="card">
                             <div class="card-body p-0">
                                 <div class="row">
-                                    <div class="col-12 p-5">
+                                    <div class="col-12 p-4">
                                         <div class="mx-auto mb-5">
                                             <a href="{{ url('/') }}">
                                                 <img src="{{ ('/frontend/assets/images/fulllogo.png') }}" alt=""
