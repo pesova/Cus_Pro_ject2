3cd327122e0f3121750c273f56257379392072a0
Douglas Okolaa <okolaadouglas@gmail.com>
2020-07-09 23:25:42 +0100

    implement all debtors

--- app/Http/Controllers/DebtorController.php
+++ app/Http/Controllers/DebtorController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Session;
 
 class DebtorController extends Controller
 {
@@ -11,9 +19,53 @@ class DebtorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('backend.debtor.index');
+        // return view('backend.debtor.index');
+        $url = env('API_URL', 'https://dev.api.customerpay.me') . '/debt';
+
+        try {
+
+            $client = new Client();
+            $payload = ['headers' => ['x-access-token' => Cookie::get('api_token')]];
+
+            $response = $client->request("GET", $url, $payload);
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode == 200) {
+                $body = $response->getBody();
+                $result = json_decode($body);
+
+                $debts = $result->data->debts;
+
+                $perPage = 10;
+                $page = null;
+                $debts = collect($debts);
+
+                $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+                $debts = $debts instanceof Collection ? $debts : Collection::make($debts);
+                $debtors = new LengthAwarePaginator($debts->forPage($page, $perPage), $debts->count(), $perPage, $page);
+                return view('backend.debtor.index', compact('debtors'));
+            }
+
+            Session::flash('message', "Temporarily unable to get all stores");
+            return view('backend.debtor.index', []);
+        } catch (RequestException $e) {
+            Log::info('Catch error: LoginController - ' . $e->getMessage());
+
+            if ($e->hasResponse()) {
+                $response = $e->getResponse()->getBody();
+                $result = json_decode($response);
+                Session::flash('message', $result->message);
+                return view('backend.debtor.index', []);
+            }
+
+            //5xx server error
+            return view('errors.500');
+        } catch (\Exception $e) {
+            Log::error('Catch error: StoreController - ' . $e->getMessage());
+            return view('errors.500');
+        }
     }
 
     /**
@@ -57,10 +109,10 @@ class DebtorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit()
+    public function edit($id)
     {
         //
-        
+
     }
 
     /**
