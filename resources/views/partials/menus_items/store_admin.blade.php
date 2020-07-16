2f733d608598a2430138d95613bd8ed32dc2aa2d
Chinaza <obiekweagnesmary@gmail.com>
2020-07-16 22:54:22 +0100

    Update store_admin.blade.php

--- resources/views/partials/menus_items/store_admin.blade.php
+++ resources/views/partials/menus_items/store_admin.blade.php
@@ -1,21 +1,21 @@
 <li>
     <a href="{{ route('customer.index') }}">
         <i class='uil uil-chat-bubble-user'></i>
-        <span class ="fourth"> Customers </span>
+        <span class="fourth"> Customers </span>
     </a>
 </li>
 
 <li>
     <a href="{{ route('transaction.index') }}">
         <i data-feather="credit-card"></i>
-        <span class ="fifth"> Transactions </span>
+        <span class="fifth"> Transactions </span>
     </a>
 </li>
 
 <li>
     <a href="{{ route('store.index') }}">
         <i class="uil uil-shop"></i>
-        <span class = "third">Stores</span>
+        <span class="third">Stores</span>
     </a>
 </li>
 
@@ -29,14 +29,14 @@
 <li>
     <a href="{{ route('debtor.index') }}">
         <i data-feather="bell"></i>
-        <span class = "sixth"> Debtors </span>
+        <span class="sixth"> Debtors </span>
     </a>
 </li>
 
 <li>
     <a href="{{ route('assistants.index') }}">
         <i data-feather="users"></i>
-        <span class = "seventh"> Assistants </span>
+        <span class="seventh"> Assistants </span>
     </a>
 </li>
 
@@ -61,7 +61,7 @@
 <li>
     <a href="{{ route('setting') }}">
         <i class="uil  uil-cog"></i>
-        <span class =second> Settings </span>
+        <span class=second> Settings </span>
     </a>
 </li>
 
