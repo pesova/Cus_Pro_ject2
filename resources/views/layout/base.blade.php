3289e60950f869a0428866376556e679e2952c7c
Jeremiah Iro <Jeremiahiro@gmail.com>
2020-07-16 18:14:43 +0100

    transaction index now working

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -24,8 +24,6 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/shepherd/2.0.0-beta.1/css/shepherd-theme-arrows.css" />
     <link href="{{asset('backend/assets/css/tourguide.css')}}" rel="stylesheet" type="text/css">
 
-
-
     <!-- Other Style CSS -->
     @yield('custom_css')
 
