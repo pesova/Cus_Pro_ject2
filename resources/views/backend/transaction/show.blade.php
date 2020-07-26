4bbe7312c7f8dd4e264b96e5e777ad755a160834
Jeremiah Iro <Jeremiahiro@gmail.com>
2020-07-26 20:53:12 +0100

    bugs for v2

--- resources/views/backend/transaction/show.blade.php
+++ resources/views/backend/transaction/show.blade.php
@@ -19,14 +19,17 @@
                                         {{ \Carbon\Carbon::parse($transaction->createdAt)->diffForhumans() }}</h6>
                                 </div>
                                 <div class="col-md-8 row text-center">
-                                    <a href="" data-toggle="modal" data-target="#sendReminderModal" class="col-md-3 offset-1 mt-1 btn btn-sm btn-warning">
+                                    <a href="" data-toggle="modal" data-target="#sendReminderModal"
+                                        class="col-md-3 offset-1 mt-1 btn btn-sm btn-warning">
                                         Send Debt Reminder <i data-feather="send"></i>
                                     </a>
                                     @if(Cookie::get('user_role') == 'store_admin')
-                                    <a href="#" class="col-md-3 offset-1 mt-1 btn btn-sm btn-primary" data-toggle="modal" data-target="#editTransactionModal">
+                                    <a href="#" class="col-md-3 offset-1 mt-1 btn btn-sm btn-primary"
+                                        data-toggle="modal" data-target="#editTransactionModal">
                                         Edit <i data-feather="edit-3"></i>
                                     </a>
-                                    <a data-toggle="modal" data-target="#deleteModal" href="" class="col-md-3 offset-1 mt-1 btn btn-sm btn-danger">
+                                    <a data-toggle="modal" data-target="#deleteModal" href=""
+                                        class="col-md-3 offset-1 mt-1 btn btn-sm btn-danger">
                                         Delete <i data-feather="delete"></i>
                                     </a>
                                     @endif
@@ -46,7 +49,8 @@
                                 <div class="col-xl-3 col-sm-6">
                                     <!-- stat 2 -->
                                     <div class="media">
-                                        <i data-feather="check-square" class="align-self-center icon-dual icon-sm mr-2"></i>
+                                        <i data-feather="check-square"
+                                            class="align-self-center icon-dual icon-sm mr-2"></i>
                                         <div class="media-body">
                                             <h6 class="mt-0 mb-0 text-capitalize">{{ $transaction->type }}</h6>
                                             <span class="text-muted">Transaction Type</span>
@@ -59,7 +63,8 @@
                                         <i data-feather="users" class="align-self-center icon-dual icon-sm mr-2"></i>
                                         <div class="media-body">
                                             <h6 class="m-0">
-                                                <a href="{{ route('customer.show', $transaction->store_ref_id->_id.'-'.$transaction->customer_ref_id->_id)}}">{{ $transaction->customer_ref_id->name }}
+                                                <a
+                                                    href="{{ route('customer.show', $transaction->store_ref_id.'-'.$transaction->customer_ref_id)}}">{{ $transaction->customer_ref->name }}
                                                 </a>
                                             </h6>
                                             <span class="text-muted">Customer Name</span>
@@ -73,7 +78,7 @@
                                         <div class="media-body">
                                             <h6 class="mt-0 mb-0">
                                                 <h6 class="m-0">
-                                                    <a href="tel:+{{ $transaction->customer_ref_id->phone_number }}">{{ $transaction->customer_ref_id->phone_number }}
+                                                    <a href="tel:+{{ $transaction->customer_ref->phone_number }}">{{ $transaction->customer_ref->phone_number }}
                                                     </a>
                                                 </h6>
                                             </h6>
@@ -97,7 +102,8 @@
                                 <div class="row">
                                     <div class="col-lg-6">
                                         <h6 class="mt-0 ">Description</h6>
-                                        <textarea name="" readonly id="" cols="auto" rows="3" sty class="form-control w-100 flex-1">{{ $transaction->description }}</textarea>
+                                        <textarea name="" readonly id="" cols="auto" rows="3" sty
+                                            class="form-control w-100 flex-1">{{ $transaction->description }}</textarea>
                                     </div>
 
                                     <div class="col-lg-6">
@@ -131,11 +137,12 @@
                                                 <h6 class="">Store Name:</h6>
                                                 <p>
                                                     @if(Cookie::get('user_role') != 'store_assistant')
-                                                    <a href="{{ route('store.show', $transaction->store_ref_id->_id)}}" class="mr-2 text-uppercase">
-                                                        {{ $transaction->store_ref_id->store_name }}
+                                                    <a href="{{ route('store.show', $transaction->store_ref_id)}}"
+                                                        class="mr-2 text-uppercase">
+                                                        {{ $transaction->store_ref->store_name }}
                                                     </a>
                                                     @else
-                                                    {{ $transaction->store_ref_id->store_name }}
+                                                    {{ $transaction->store_ref->store_name }}
                                                     @endif
                                                 </p>
                                             </div>
@@ -145,16 +152,23 @@
                                                 </h6>
                                                 <label class="switch">
                                                     @if(Cookie::get('user_role') != 'store_assistant') disabled
-                                                    <input type="checkbox" id="togBtn" {{ $transaction->status == true ? 'checked' : '' }} data-id="{{ $transaction->_id }}" data-store="{{ $transaction->store_ref_id->_id }}" data-customer="{{ $transaction->customer_ref_id->_id}}">
+                                                    <input type="checkbox" id="togBtn"
+                                                        {{ $transaction->status == true ? 'checked' : '' }}
+                                                        data-id="{{ $transaction->_id }}"
+                                                        data-store="{{ $transaction->store_ref_id }}"
+                                                        data-customer="{{ $transaction->customer_ref_id}}">
                                                     @else
-                                                    <input type="checkbox" id="togBtn" {{ $transaction->status == true ? 'checked' : '' }} disabled>
+                                                    <input type="checkbox" id="togBtn"
+                                                        {{ $transaction->status == true ? 'checked' : '' }} disabled>
                                                     @endif
 
                                                     <div class="slider round">
                                                         <span class="on">Paid</span><span class="off">Pending</span>
                                                     </div>
                                                 </label>
-                                                <div id="statusSpiner" class="spinner-border spinner-border-sm text-primary d-none" role="status">
+                                                <div id="statusSpiner"
+                                                    class="spinner-border spinner-border-sm text-primary d-none"
+                                                    role="status">
                                                     <span class="sr-only">Loading...</span>
                                                 </div>
                                             </div>
@@ -175,7 +189,7 @@
                 </div>
                 <div class="card-body">
                     <div class="table-responsive table-data">
-                        <table id="transactionTable" class="table table-striped table-bordered" style="width:100%">
+                        <table id="debtReminders" class="table table-striped table-bordered" style="width:100%">
                             <thead>
                                 <tr>
                                     <th>Ref ID</th>
@@ -195,7 +209,9 @@
                                     <td><span class="badge badge-success">{{ $debt->status }}</span></td>
                                     <td>{{ \Carbon\Carbon::parse($debt->createdAt)->diffForhumans() }}</td>
                                     <td>
-                                        <a href="" data-toggle="modal" onclick="return previousMessage('{{ $debt->message }}')" data-target="#ResendReminderModal" class="btn btn-primary btn-sm mt-2">
+                                        <a href="" data-toggle="modal"
+                                            onclick="return previousMessage('{{ $debt->message }}')"
+                                            data-target="#ResendReminderModal" class="btn btn-primary btn-sm mt-2">
                                             Resend
                                         </a>
                                     </td>
@@ -229,17 +245,18 @@
 @endsection
 
 @section('javascript')
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/jszip-2.5.0/dt-1.10.21/b-1.6.2/b-html5-1.6.2/datatables.min.js">
+<script type="text/javascript"
+    src="https://cdn.datatables.net/v/bs4/jq-3.3.1/jszip-2.5.0/dt-1.10.21/b-1.6.2/b-html5-1.6.2/datatables.min.js">
 </script>
 
 <script src="{{ asset('/backend/assets/js/textCounter.js')}}"></script>
 
 <script>
-    jQuery(function($) {
+    jQuery(function ($) {
         const token = "{{Cookie::get('api_token')}}"
         const host = "{{ env('API_URL', 'https://dev.api.customerpay.me') }}";
 
-        $('#togBtn').change(function() {
+        $('#togBtn').change(function () {
             $(this).attr("disabled", true);
             $('#statusSpiner').removeClass('d-none');
 
@@ -249,17 +266,17 @@
             let _customer_id = $(this).data('customer');
 
             $.ajax({
-                url: `${host}/transaction/update/${id}`
-                , headers: {
+                url: `${host}/transaction/update/${id}`,
+                headers: {
                     'x-access-token': token
-                }
-                , data: {
-                    store_id: store
-                    , status: _status
-                    , customer_id: _customer_id
-                , }
-                , type: 'PATCH'
-            , }).done(response => {
+                },
+                data: {
+                    store_id: store,
+                    status: _status,
+                    customer_id: _customer_id,
+                },
+                type: 'PATCH',
+            }).done(response => {
                 if (response.success != true) {
                     $(this).prop("checked", !this.checked);
                     $('#error').show();
@@ -289,7 +306,9 @@
 
     // pagination for debts
     $(() => {
-        $('#transactionTable').DataTable({});
+        $('#debtReminders').DataTable({
+         
+        });
     });
 
 </script>
