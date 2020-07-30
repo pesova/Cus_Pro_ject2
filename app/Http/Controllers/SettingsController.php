dbdd813266e6cb6fae60d1c26dba52adc2a8946e
Douglas Okolaa <okolaadouglas@gmail.com>
2020-07-30 14:15:20 +0100

    UI for finace settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -10,6 +10,7 @@ use function GuzzleHttp\json_encode;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cookie;
 use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Session;
 
 class SettingsController extends Controller
 {
@@ -17,34 +18,66 @@ class SettingsController extends Controller
     public $headers;
     public $user_id;
 
+    public function __construct()
+    {
+        $this->user_id = Cookie::get('user_id');
+    }
+
     // Controller action to display settings page.
     public function index()
     {
-
-        // Setting header
+        // User Data
         $user_details = [];
-        Cookie::get('user_id') !== null ?  $user_details['id'] = Cookie::get('user_id') : "";
-        Cookie::get('email') !== null ?  $user_details['email'] = Cookie::get('email') : "";
-        Cookie::get('first_name') !== null ?  $user_details['first_name'] = Cookie::get('first_name') : "";
-        Cookie::get('last_name') !== null ?  $user_details['last_name'] = Cookie::get('last_name') : "";
-        Cookie::get('account_name') !== null ?  $user_details['account_name'] = Cookie::get('account_name') : "";
-        Cookie::get('account_number') !== null ?  $user_details['account_number'] = Cookie::get('account_number') : "";
-        Cookie::get('bank') !== null ?  $user_details['bank'] = Cookie::get('bank') : "";
-        Cookie::get('phone_number') !== null ?  $user_details['phone_number'] = Cookie::get('phone_number') : "";
-        Cookie::get('is_active') !== null ?  $user_details['is_active'] = Cookie::get('is_active') : "";
+        $user_details['id'] = Cookie::get('user_id');
+        $user_details['email'] = Cookie::get('email');
+        $user_details['first_name'] = Cookie::get('first_name');
+        $user_details['last_name'] = Cookie::get('last_name');
+        $user_details['account_name'] = Cookie::get('account_name');
+        $user_details['account_number'] = Cookie::get('account_number');
+        $user_details['account_bank'] = Cookie::get('account_bank');
+        $user_details['phone_number'] = Cookie::get('phone_number');
+        $user_details['is_active'] = Cookie::get('is_active');
+
+        $url= env('API_URL', 'https://dev.api.customerpay.me') . '/banks/list';
+        try {
+            $client = new Client();
+            $headers = ['headers' => ['x-access-token' => Cookie::get('api_token')]];
+            $bank_list = $client->request('GET', $url, $headers);
+            $statusCode = $bank_list->getStatusCode();
+
+            if ($statusCode == 200) {
+                $bank_list = json_decode($bank_list->getBody())->data;
+            }
+        } catch (RequestException $e) {
+            Log::info('Catch error: settingsController - ' . $e->getMessage());
+            // token expired
+            if ($e->getCode() == 401) {
+                Session::flash('message', 'session expired');
+                return redirect()->route('logout');
+            }
+            // get response to catch 4 errors
+            if ($e->hasResponse()) {
+                $response = $e->getResponse()->getBody();
+                $result = json_decode($response);
+                Session::flash('message', 'could not retrieve bank list');
+                return back();
+            }
+        } catch (Exception $e) {
+            // token expired
+            if ($e->getCode() == 401) {
+                Session::flash('message', 'session expired');
+                return redirect()->route('logout');
+            }
+            Log::error('Catch error: seetingsController - ' . $e->getMessage());
+            return view('errors.500');
+        }
+
         return view('backend.settings.settings')->with("user_details", $user_details);
-        // Setting User_id
-        $this->user_id = Cookie::get('user_id');
     }
 
     // Controller action to update user details.
     public function update(Request $request)
     {
-
-        // Setting header
-
-        // Setting User_id
-        $this->user_id = Cookie::get('user_id');
 
         try {
             // check if all fields are available
@@ -90,9 +123,9 @@ class SettingsController extends Controller
                         $user_detail_res = json_decode($response->getBody(), true);
                         $filtered_user_detail = $user_detail_res['data']['store_admin']['local'];
                         $bank_details = $user_detail_res['data']['store_admin']['bank_details'];
-                        $bank_details['account_name'] = isset($bank_details['account_name'])? $bank_details['account_name']:"";
-                        $bank_details['account_number'] = isset($bank_details['account_number'])? $bank_details['account_number']:"";
-                        $bank_details['bank'] = isset($bank_details['bank'])? $bank_details['bank']:"";
+                        $bank_details['account_name'] = isset($bank_details['account_name']) ? $bank_details['account_name'] : "";
+                        $bank_details['account_number'] = isset($bank_details['account_number']) ? $bank_details['account_number'] : "";
+                        $bank_details['bank'] = isset($bank_details['bank']) ? $bank_details['bank'] : "";
                         $user_details = [
                             "email" => $filtered_user_detail['email'],
                             "phone_number" => $filtered_user_detail['phone_number'],
@@ -116,7 +149,7 @@ class SettingsController extends Controller
                     }
                     if ($control == 'password_change') {
                         $request->session()->flash('message', "Password updated successfully");
-                        return redirect(route('setting').'#change-password');
+                        return redirect(route('setting') . '#change-password');
                     }
                 }
             } else {
@@ -135,7 +168,7 @@ class SettingsController extends Controller
             }
 
             if ($control == 'password_change') {
-                return redirect(route('setting').'#change-password');
+                return redirect(route('setting') . '#change-password');
             }
             if ($control == 'profile_update') {
                 return redirect()->route('setting');
