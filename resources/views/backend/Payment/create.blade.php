578cbc75776a62b10b670a4f20a6975323d76be4
Jeremiah Iro <Jeremiahiro@gmail.com>
2020-07-30 14:08:42 +0100

    payment view

---
+++ resources/views/backend/Payment/create.blade.php
@@ -0,0 +1,64 @@
+@extends('backend.payment.layout.app')
+
+@section('content')
+<div>
+    <h4 class="font-weight-bold text-center">Payment</h4>
+
+    <div class="py-3">
+        <div class="d-flex justify-content-between align-items-center">
+            <span>Total</span>
+            <span>$6,700</span>
+        </div>
+        <div class="row p-2 text-center">
+            <a href="" class="col w-1/2 rounded-left py-3 bg-primary text-white">
+                <img src="{{ asset('frontend/assets/images/payment/vector.svg')}}" alt="" class="inline"> Pay with
+                Card</a>
+            <a href="" class="col w-1/2 rounded-right py-3 btn-light text-dark">Pay with Transfer</a>
+        </div>
+    </div>
+
+    <form action="#" class="parsley-examples">
+        <div class="form-group mb-2">
+            <label for="card_number" class="m-0 px-2">Card Number<span class="text-danger">*</span></label>
+            <input type="text" name="card_number" parsley-trigger="change" required placeholder="0000 0000 0000 0000"
+                class="form-control form-control-lg" id="card_number">
+        </div>
+        <div class="form-group mb-2">
+            <label for="card_holder_name" class="m-0 px-2">Card Holder's Name<span class="text-danger">*</span></label>
+            <input type="email" name="card_holder_name" parsley-trigger="change" required placeholder=""
+                class="form-control form-control-lg" id="card_holder_name">
+        </div>
+        <div class="row">
+            <div class="col-8">
+                <label for="card_expiry_month" class="m-0 px-2">Expiry Date<span class="text-danger">*</span></label>
+                <div class="d-flex">
+                    <div class="form-group">
+                        <input id="card_expiry_month" type="number" max="2" name="card_expiry_month" placeholder="MM"
+                            required class="form-control form-control-lg">
+                    </div>
+                    <div class="form-group">
+                        <input id="card_expiry_year" type="number" max="4" name="card_expiry_year" placeholder="YYYY"
+                            required class="form-control form-control-lg">
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="">
+                    <div class="form-group">
+                        <label for="card_cvv" class="m-0 px-2">CVV <span class="text-danger">*</span></label>
+                        <input name="card_cvv" id="card_cvv" type="password" required placeholder="cvv"
+                            class="form-control form-control-lg" id="card_cvv">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group mt-5 mb-0">
+            <button class="btn btn-primary btn-lg w-100" type="submit">
+                Pay $6,700
+            </button>
+        </div>
+
+    </form>
+</div>
+@endsection
---
+++ resources/views/backend/Payment/index.blade.php
@@ -0,0 +1,37 @@
+@extends('backend.payment.layout.app')
+
+@section('content')
+<div class="">
+    <div class="d-flex">
+        <h4>From:</h4>
+        <div class="ml-2">
+            <h5 class="text-primary">Iro Stores</h5>
+            <h5 class="text-grey">+2348136478020</h5>
+        </div>
+    </div>
+
+    <div class="text-center py-3">
+        <p>Mollit exercitation consectetur dolor occaecat sit elit fugiat ullamco duis non magna cupidatat.</p>
+    </div>
+
+    <div class="p-5 text-center shadow-lg rounded">
+        <h5 class="py-3 text-uppercase">Payment Reminder</h5>
+        <h5 class="py-3 text-danger">$6,700</h5>
+        <p>Occaecat aliqua anim sint anim sunt anim ut ea nulla dolore.</p>
+    </div>
+    <div class="">
+        <a href="{{ route('pay.create') }}" class="my-3 btn btn-lg btn-primary w-100 text-white font-weight-bold">Pay
+            Now</a>
+        <a href="{{ route('pay') }}"
+            class="my-3 btn btn-lg border border-primary w-100 text-primary font-weight-bold">Remind Me Later</a>
+    </div>
+
+    <div class="text-center p-3">
+        <img src="{{ asset('frontend/assets/images/payment/check-circle.svg')}}" alt="Verified" />
+
+        <p class="text-primary py-2">Verified by myCustomer</p>
+        <img src="{{ asset('frontend/assets/images/payment/android-button.svg')}}" alt="Google Play" />
+    </div>
+</div>
+
+@endsection
