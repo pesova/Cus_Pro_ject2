79b1a0314b6b23a047e311e653d307c459ed525f
Isiko saidiali <isikosaidiali@gmail.com>
2020-08-04 22:58:18 +0300

    refactored the UI

--- resources/views/partials/dashboard/store_admin.blade.php
+++ resources/views/partials/dashboard/store_admin.blade.php
@@ -19,7 +19,7 @@
             <div class="card-body pt-0">
                 <div class="row">
                     <div class="col-sm-4">
-                        <div class="mb-4">
+                        <div class="avatar-md profile-user-wid mb-4">
                             @php
                             $profile_picture = Cookie::get('profile_picture');
                             $profile_picture = rtrim($profile_picture);
