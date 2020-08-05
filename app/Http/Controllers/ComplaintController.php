e9e4e992cf7adefba07195e3f88e135161a137ae
Douglas Okolaa <okolaadouglas@gmail.com>
2020-08-05 10:08:07 +0100

    fix and optimize complaint feature

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Support\Facades\Log;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Session;
+use stdClass;
 
 class ComplaintController extends Controller
 {
@@ -22,33 +25,68 @@ class ComplaintController extends Controller
         $url_s = env('API_URL', 'https://dev.api.customerpay.me') . "/complaints/all";
         $user_role = Cookie::get('user_role');
 
-        $client = new Client();
+        try {
+            $client = new Client();
 
-        $headers = [
-            'headers' => [
-                'x-access-token' => Cookie::get('api_token')
-            ]
-        ];
-        if ($user_role == 'super_admin'){
-            $response = $client->request('GET', $url_s, $headers);
-        }else{
-            $response = $client->request('GET', $url, $headers);
-        }
+            $headers = [
+                'headers' => [
+                    'x-access-token' => Cookie::get('api_token')
+                ]
+            ];
+            if ($user_role == 'super_admin') {
+                $response = $client->request('GET', $url_s, $headers);
+            } else {
+                $response = $client->request('GET', $url, $headers);
+            }
 
-        $statusCode = $response->getStatusCode();
+            $statusCode = $response->getStatusCode();
 
-        if ($statusCode == 200) {
+            if ($statusCode == 200) {
 
-            $body = $response->getBody()->getContents();
-            $complaints = json_decode($body);
+                $body = $response->getBody()->getContents();
+                $complaints = json_decode($body);
+                return view('backend.complaints.index')->with('responses', $complaints);
+            } elseif ($statusCode == 401) {
+
+                return redirect()
+                    ->route('login')
+                    ->with('message', "Please Login Again");
+            }
+        } catch (RequestException $e) {
+            // token expired
+            if ($e->getCode() == 401) {
+                Session::flash('message', 'session expired');
+                return redirect()->route('logout');
+            }
+
+            // get response to catch 4 errors
+            if ($e->hasResponse()) {
+                $response = $e->getResponse()->getBody();
+                $result = json_decode($response);
+                Session::flash('message', isset($result->message) ? $result->message : $result->Message);
+            }
+            
+            $complaints = new stdClass;
+            $complaints->complaintCounts = new stdClass;
+            if (is_super_admin()) {
+                $complaints->data = [];
+            } else {
+                $complaints->data = new stdClass;
+                $complaints->data->complaints =[];
+            }
+            $complaints->complaintCounts->resolved = 0;
+            $complaints->complaintCounts->pending = 0;
             return view('backend.complaints.index')->with('responses', $complaints);
-        } elseif ($statusCode == 401) {
+        } catch (Exception $e) {
+            // token expired
+            if ($e->getCode() == 401) {
+                Session::flash('message', 'session expired');
+                return redirect()->route('logout');
+            }
 
-            return redirect()
-                ->route('login')
-                ->with('message', "Please Login Again");
+            Log::error('Catch error: ComplaintController - ' . $e->getMessage());
+            return view('errors.500');
         }
-        // return view('backend.complaints.index');
     }
 
     /**
@@ -90,7 +128,7 @@ class ComplaintController extends Controller
                 "form_params" => [
                     "message" => $request->input('message'),
                     "subject" => $request->input('subject'),
-                    "name" => $firstname." ".$lastname,
+                    "name" => $firstname . " " . $lastname,
                     "email" => $email
                 ]
             ];
@@ -119,15 +157,14 @@ class ComplaintController extends Controller
                 // get response to catch 4xx errors
                 $response = json_decode($e->getResponse()->getBody());
                 $request->session()->flash('error', "Make sure all fields are filled .\n Make sure the description is more than 10 characters");
-                return back();
             }
             // check for 500 server error
-            return view('errors.500');
-        } catch (\Exception $e) {
+            return back();
+        } catch (Exception $e) {
             //log error;
             Log::error('Catch error: ComplaintController - ' . $e->getMessage());
             $request->session()->flash('error', 'Could not connect to the server. Please try again later');
-            return back();
+            return view('errors.500');
         }
     }
 
@@ -161,15 +198,25 @@ class ComplaintController extends Controller
 
                 return view('backend.complaints.show')->with('response', $complaints);
             }
-            if ($statusCode == 500) {
+        } catch (RequestException $e) {
+            //log error;
+            Log::error('Catch error: ComplaintController - ' . $e->getMessage());
 
-                return view('errors.500');
+            if ($e->hasResponse()) {
+                // get response to catch 4xx errors
+                $response = json_decode($e->getResponse()->getBody());
+                Session::flash('error', isset($response->message) ? $response->message : $response->Message);
             }
-        } catch (\Exception $e) {
-
+            return back();
+         } catch (Exception $e) {
+            // token expired
+            if ($e->getCode() == 401) {
+                Session::flash('message', 'session expired');
+                return redirect()->route('logout');
+            }
+            Log::error('Catch error: ComplaintController - ' . $e->getMessage());
             return view('errors.500');
         }
-        // return view('backend.complaints.index');
     }
 
     /**
@@ -185,7 +232,7 @@ class ComplaintController extends Controller
 
         try {
 
-            if ( Cookie::get('user_role') == "super_admin") {
+            if (Cookie::get('user_role') == "super_admin") {
 
                 $client = new Client();
 
@@ -209,11 +256,10 @@ class ComplaintController extends Controller
                 }
             } else {
 
-                \Session::flash('error', 'You do not have access to do this');
+                Session::flash('error', 'You do not have access to do this');
                 return back();
             }
-
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
 
             return view('errors.500');
         }
