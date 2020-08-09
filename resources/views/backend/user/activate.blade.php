cba087c6a4f458981bde959e1b16ae0963e8aeba
Isiko saidiali <isikosaidiali@gmail.com>
2020-08-09 08:11:43 +0300

    init

--- resources/views/backend/user/activate.blade.php
+++ resources/views/backend/user/activate.blade.php
@@ -42,7 +42,7 @@
                                         <form action="#" class="authentication-form">
                                             <div class="form-group mt-4">
                                                 <label class="form-control-label">Enter 6 digit code</label>
-                                                <a href="" class="float-right text-muted text-unline-dashed ml-1" id="resend_code">resend code</a>
+                                                <a href="" class="float-right text-muted text-unline-dashed ml-1" id="resend_code">Resend code</a>
                                                 <div class="input-group input-group-merge">
                                                     <div class="input-group-prepend">
                                                         <span class="input-group-text">
