513194e7f305034e9bfdee8022d7dba95b0f168d
Kofi Mokome <kofimokome10@gmail.com>
2020-08-15 15:27:05 +0100

    Remove select store from create transaction and create customer modal is user is store admin

--- resources/views/partials/bsidebar.blade.php
+++ resources/views/partials/bsidebar.blade.php
@@ -16,7 +16,6 @@
 
                 @if(\Illuminate\Support\Facades\Cookie::get('user_role') == 'super_admin')
                     {{-- super admin protected routes here --}}
-                    @include('partials.menus_items.store_admin')
                     @include('partials.menus_items.super_admin')
                 @endif
 
