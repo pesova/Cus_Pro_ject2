520cf3fd9ce518b2498a0b77affa9deb4b42d93e
Kofi Mokome <kofimokome10@gmail.com>
2020-08-16 15:09:53 +0100

    fix navbar not clicking

--- resources/views/partials/bheader.blade.php
+++ resources/views/partials/bheader.blade.php
@@ -5,9 +5,9 @@
         <a href="{{route('home') }}" class="navbar-brand mr-0 mr-md-2 logo">
             <span class="logo-lg">
                 @if(\Cookie::get('theme') == 'dark')
-                <img src="{{('/frontend/assets/images/fulllogo.png')}}" alt="" height="48" />
+                    <img src="{{('/frontend/assets/images/fulllogo.png')}}" alt="" height="48"/>
                 @else
-                <img src="{{('/frontend/assets/images/fulllogo.png')}}" alt="" height="48" />
+                    <img src="{{('/frontend/assets/images/fulllogo.png')}}" alt="" height="48"/>
                 @endif
             </span>
             <span class="logo-sm">
@@ -26,24 +26,28 @@
 
         <ul class="navbar-nav flex-row ml-auto d-flex list-unstyled topnav-menu float-right mb-0">
 
-            <li class="pointer" data-toggle="dropdown" href="#" role="button" aria-haspopup="false"
-                aria-expanded="false">
+            <li class="pointer">
+
                 <div class="media user-profile mt-2 mb-2">
+                    <div data-toggle="dropdown" href="#" role="button" aria-haspopup="false"
+                         aria-expanded="false"
+                         style="position:absolute;top:0; width: 100%; height:100%;">
+                    </div>
                     <object data="{{ get_profile_picture() }}" type="image/jpg" class="avatar-sm rounded-circle mr-2">
                         <img src="/backend/assets/images/users/default.png" class="avatar-sm rounded-circle mr-2"
-                            alt="Profile Picture" />
+                             alt="Profile Picture"/>
                     </object>
                     <div class="media-body">
                         @if(is_store_assistant())
 
-                        <h6 class="pro-user-name mt-0 mb-0">{{Cookie::get('name')}}
-                        </h6>
+                            <h6 class="pro-user-name mt-0 mb-0">{{Cookie::get('name')}}
+                            </h6>
                         @else
 
-                        <h6 class="pro-user-name mt-0 mb-0">
-                            {{Cookie::get('first_name') == 'Not set'? '':Cookie::get('first_name')}}
-                            {{Cookie::get('last_name')== 'Not set'? '':Cookie::get('last_name')}}
-                        </h6>
+                            <h6 class="pro-user-name mt-0 mb-0">
+                                {{Cookie::get('first_name') == 'Not set'? '':Cookie::get('first_name')}}
+                                {{Cookie::get('last_name')== 'Not set'? '':Cookie::get('last_name')}}
+                            </h6>
                         @endif
                         <span class="pro-user-desc">
                             {{format_role_name()}}
@@ -51,7 +55,7 @@
                     </div>
                     <div class="dropdown align-self-center profile-dropdown-menu">
                         <a class="dropdown-toggle mr-0" data-toggle="dropdown" href="#" role="button"
-                            aria-haspopup="false" aria-expanded="false">
+                           aria-haspopup="false" aria-expanded="false">
                             <span data-feather="chevron-down"></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right profile-dropdown">
@@ -63,15 +67,15 @@
 
                             <div class="dropdown-divider"></div>
                             @if(\Cookie::get('theme') == 'dark')
-                            <a href="{{route('theme.change','light')}}" class="dropdown-item notify-item">
-                                <i data-feather="sun" class="icon-dual icon-xs mr-2"></i>
-                                <span>Switch to light mode</span>
-                            </a>
+                                <a href="{{route('theme.change','light')}}" class="dropdown-item notify-item">
+                                    <i data-feather="sun" class="icon-dual icon-xs mr-2"></i>
+                                    <span>Switch to light mode</span>
+                                </a>
                             @else
-                            <a href="{{route('theme.change','dark')}}" class="dropdown-item notify-item">
-                                <i data-feather="moon" class="icon-dual icon-xs mr-2"></i>
-                                <span>Switch to dark mode</span>
-                            </a>
+                                <a href="{{route('theme.change','dark')}}" class="dropdown-item notify-item">
+                                    <i data-feather="moon" class="icon-dual icon-xs mr-2"></i>
+                                    <span>Switch to dark mode</span>
+                                </a>
                             @endif
 
                             <div class="dropdown-divider"></div>
