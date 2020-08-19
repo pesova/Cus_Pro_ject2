2f87841ce9bae73e1ac1659cca54c1b85bc4a307
Douglas Okolaa <okolaadouglas@gmail.com>
2020-08-19 23:55:35 +0100

    customer endpoint implementtion

--- app/Http/Controllers/Api/V1/CustomersController.php
+++ app/Http/Controllers/Api/V1/CustomersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Models\Admins;
+use App\Http\Resources\CustomerCollection;
 use App\Models\Customers;
 use Illuminate\Http\Request;
 
@@ -18,9 +18,26 @@ class CustomersController extends Controller
         $this->user_role = $request['request_user_role'];
     }
 
-    public function index()
+    public function index(Request $request, $store_id)
     {
+        $start      =  $request->get('page') ?? 0;
+        $length     = $request->get('length') ?? 10;
+        $draw       = $request->get('draw') ?? 1;
+        $search     = $request->get('search');
 
-        // dd(Customers::first());
+        $result = Customers::ofStore($store_id)
+            ->search($search['value'])
+            ->skip($start)
+            ->take($length)
+            ->get();
+
+        $recordsTotal = Customers::ofStore($store_id)->count();
+
+        $result = (new CustomerCollection($result))
+            ->additional([
+                'draw' => $draw + 1,
+                'recordsTotal' => $recordsTotal,
+            ]);
+        return $result;
     }
 }
--- app/Http/Middleware/ApiAuth.php
+++ app/Http/Middleware/ApiAuth.php
@@ -23,18 +23,20 @@ class ApiAuth
     public function handle($request, Closure $next)
     {
         if (!$request->headers->has('x-access-token')) {
+            $api_token = $request->header('x-access-token');
+        } elseif ($request->cookies->has('api_token')) {
+            $api_token = $request->cookie('api_token');
+        } else {
             return response()->json($this->unauthorizedResponse, 401);
         }
 
-        $api_token = $request->header('x-access-token');
-
-        $user = Admins::where('api_token', $api_token)->firstor(function ($api_token) {
-            $user = Assistants::where('api_token', $api_token)->firstor(function () {
-                return response()->json($this->unauthorizedResponse, 401);
-            });
-
-            return $user;
+        $user = Admins::where('api_token', $api_token)->firstor(function () use ($api_token) {
+            return Assistants::where('api_token', $api_token)->first();
         });
+
+        if (!$user) {
+            return response()->json($this->unauthorizedResponse, 401);
+        }
 
         $request->merge([
             'request_user_id' => $user->_id,
